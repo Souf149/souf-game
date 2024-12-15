88dff16f9d2f18850023b10dbf1b90e3b1415137
soufyan.abdellati <abdellatis149@gmail.com>
2024-12-15 09:28:52 +0100

    added leaderboard support to twitch bot

--- souf_twitch_bot/souf_game/api.py
+++ souf_twitch_bot/souf_game/api.py
@@ -1,3 +1,5 @@
+import multiprocessing
+from multiprocessing.context import Process
 from fastapi import FastAPI
 from pydantic import BaseModel
 from uvicorn import Config, Server
@@ -5,23 +7,31 @@ from souf_game.config import settings
 from souf_game.game import Game
 
 
-class Api:
-    def __init__(self):
+class Api(Process):
+    def __init__(self, app):
         if settings.game_api.port is None:
             raise Exception("PORT IS NOT SET")
-        self.app = FastAPI()
+
+        super().__init__()
+        self.app = app
         self.server = Server(
             config=Config(
                 self.app, host=settings.game_api.host, port=settings.game_api.port
             )
         )
 
+    def stop(self) -> None:
+        self.terminate()
+
     def run(self):
         self.server.run()
 
 
-api = Api()
+app = FastAPI()
+api = Api(app)
 game = Game()
+api.run()
+game.run()
 
 
 class MessageEvent(BaseModel):
@@ -29,6 +39,6 @@ class MessageEvent(BaseModel):
     username: str
 
 
-@api.app.post("/message", status_code=200)
+@app.post("/message", status_code=200)
 def new_message(messageEvent: MessageEvent) -> None:
     game.new_message(messageEvent.message, messageEvent.username)
--- souf_twitch_bot/souf_game/game.py
+++ souf_twitch_bot/souf_game/game.py
@@ -1,6 +1,63 @@
+import sched
+import sys
+import time
+import pygame
+import random
+
+background = pygame.Surface((800, 600))
+background.fill(pygame.Color("#05D810FF"))
+
+
 class Game:
     def run(self):
         print("Game is running")
 
+        self.x = random.randint(0, 50)
+        self.y = random.randint(0, 50)
+        
+        
+        
+        pygame.init()
+        pygame.display.set_caption("Quick Start")
+        self.window_surface = pygame.display.set_mode((800, 600))
+        pygame.display.update()
+        self.my_scheduler = sched.scheduler(time.time, time.sleep)
+        self.my_scheduler.enter(
+            1,
+            1,
+            self.draw,
+        )
+        self.my_scheduler.run()
+
     def new_message(self, message: str, username: str):
-        print(message)
+        self.x = random.randint(0, 50)
+        self.y = random.randint(0, 50)
+        
+        
+        
+        
+
+    def draw(self):
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+        print("HELLO")
+        self.window_surface.blit(background, (0, 0))
+        pygame.draw.rect(background, "#FF0000", pygame.Rect(self.x, self.y, 50, 50))
+
+        pygame.display.update()
+
+        self.my_scheduler.enter(
+            0.1,
+            1,
+            self.draw,
+        )
+game = Game()
+game.run()
+game.draw()
+while True:
+    time.sleep(1)    
+    game.new_message("hello there","souf")
+    
+    
+    
